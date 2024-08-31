c30c3df9c19692318e8d8ba16726d7730c7eb304
VadimWart <wartvadim@gmail.com>
2024-08-31 20:44:25 +0200

    cloud_class_js

--- classes/world.class.js
+++ classes/world.class.js
@@ -2,15 +2,42 @@ class World {
 
     character = new Character();
     enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    canvas;
+    ctx;
 
 
+
+    constructor(){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas; // greift auf globale variable canvas nicht in constructor
+        this.draw();
+    }
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        // darstellung von character, enemies, und clouds
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy =>{
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        this.clouds.forEach(cloud =>{
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+        });
+
+        // draw() wird immer wieder aufgerufen
+        let self = this; // erkennt keine this also muss in variable this einpacken
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
--- js/game.js
+++ js/game.js
@@ -1,18 +1,13 @@
 let canvas;
-let ctx;
-let world = new World();
+let world;
+
 
 
 function init(){
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
 
     console.log('My Character is', world.character);
-
-    // setTimeout( function() {
-    //     ctx.drawImage(character, 20, 20, 50, 150);
-    // }, 2000)
-    
 
 }
