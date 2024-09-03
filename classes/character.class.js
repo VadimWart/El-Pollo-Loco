61006772ead98d702f996b85bddb0e6c9ccb98a7
VadimWart <wartvadim@gmail.com>
2024-09-03 18:07:52 +0200

    level_character_sound

--- classes/character.class.js
+++ classes/character.class.js
@@ -12,6 +12,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/');
 
 
     constructor(){
@@ -24,16 +25,16 @@ class Character extends MovableObject {
     animate(){
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
             }
 
-            if (this.world.keyboard.LEFT) {
+            if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }
-            this.world.camera_x = -this.x;
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
