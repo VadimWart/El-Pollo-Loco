9a02c30f6e19a74e26eb1dd0260fe8fa0a3b0392
VadimWart <wartvadim@gmail.com>
2024-09-04 13:10:34 +0200

    character_gravity

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-    y = 150;
+    y = 50;
     height = 280;
     width = 150;
     speed = 10;
@@ -18,7 +18,7 @@ class Character extends MovableObject {
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
-
+        this.applyGravity();
         this.animate();
     }
 
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,6 +8,21 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1;
+
+    applyGravity(){
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround(){
+        return this.y < 140;
+    }
 
     loadImage(path) {
         this.img = new Image();
