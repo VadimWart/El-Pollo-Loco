3ebc4f8c63e5583b7f6c2c42eae149f7ec6d0d99
VadimWart <wartvadim@gmail.com>
2024-09-05 19:00:51 +0200

    sound_js_update

--- classes/sounds.class.js
+++ classes/sounds.class.js
@@ -1,6 +1,14 @@
 class AudioManager {
     constructor() {
         this.walking_sound = new Audio('audio/running.mp3');
+        this.background_sound = new Audio('audio/background_sound.mp3');
+        this.chicken_sound = new Audio('audio/chicken_dead.mp3');
+        this.endboss_sound = new Audio('audio/endboss_dead.mp3');
+        this.walking_sound = new Audio('audio/running.mp3');
+        this.walking_sound = new Audio('audio/running.mp3');
+        this.walking_sound = new Audio('audio/running.mp3');
+        this.walking_sound = new Audio('audio/running.mp3');
+        this.walking_sound = new Audio('audio/running.mp3');
     }
 
     playWalkingSound() {
