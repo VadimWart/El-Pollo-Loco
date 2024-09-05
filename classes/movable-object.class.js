503929e6029490f478ff1519b16b043a1ed00c32
VadimWart <wartvadim@gmail.com>
2024-09-05 13:02:30 +0200

    restructuring_structur

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {}; // JSON/Objekt um bilder abzuspeichern, charachter und chicken animieren
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -25,15 +18,6 @@ class MovableObject {
 
     isAboveGround(){
         return this.y < 140;
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
@@ -73,18 +57,6 @@ class MovableObject {
 
     isDead() {
         return this.energy == 0;
-    }
-
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ... ] 
-     */
-    loadImages(arr){
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
