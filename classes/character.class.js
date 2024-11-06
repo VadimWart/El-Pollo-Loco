b4ed69057fd84d185a287d3aa4217ce523032aa7
VadimWart <wartvadim@gmail.com>
2024-11-06 17:04:27 +0100

    init_level1_update

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,153 +1,152 @@
 class Character extends MovableObject {
-    y = 50;
-    height = 280;
-    width = 150;
-    speed = 10;
-    lastMove = Date.now();
-    
-    IMAGES_WALKING = [
-        'img/2_character_pepe/2_walk/W-21.png',
-        'img/2_character_pepe/2_walk/W-22.png',
-        'img/2_character_pepe/2_walk/W-23.png',
-        'img/2_character_pepe/2_walk/W-24.png',
-        'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png'
-    ];
+  y = 50;
+  height = 280;
+  width = 150;
+  speed = 10;
+  lastMove = Date.now();
 
-    IMAGES_JUMPING = [
-        'img/2_character_pepe/3_jump/J-31.png',
-        'img/2_character_pepe/3_jump/J-32.png',
-        'img/2_character_pepe/3_jump/J-33.png',
-        'img/2_character_pepe/3_jump/J-34.png',
-        'img/2_character_pepe/3_jump/J-35.png',
-        'img/2_character_pepe/3_jump/J-36.png',
-        'img/2_character_pepe/3_jump/J-37.png',
-        'img/2_character_pepe/3_jump/J-38.png',
-        'img/2_character_pepe/3_jump/J-39.png'
-    ];
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
 
-    IMAGES_DEAD = [
-        'img/2_character_pepe/5_dead/D-51.png',
-        'img/2_character_pepe/5_dead/D-52.png',
-        'img/2_character_pepe/5_dead/D-53.png',
-        'img/2_character_pepe/5_dead/D-54.png',
-        'img/2_character_pepe/5_dead/D-55.png',
-        'img/2_character_pepe/5_dead/D-56.png',
-        'img/2_character_pepe/5_dead/D-57.png'
-    ];
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
 
-    IMAGES_HURT = [
-        'img/2_character_pepe/4_hurt/H-41.png',
-        'img/2_character_pepe/4_hurt/H-42.png',
-        'img/2_character_pepe/4_hurt/H-43.png'
-    ];
+  IMAGES_DEAD = [
+    "img/2_character_pepe/5_dead/D-51.png",
+    "img/2_character_pepe/5_dead/D-52.png",
+    "img/2_character_pepe/5_dead/D-53.png",
+    "img/2_character_pepe/5_dead/D-54.png",
+    "img/2_character_pepe/5_dead/D-55.png",
+    "img/2_character_pepe/5_dead/D-56.png",
+    "img/2_character_pepe/5_dead/D-57.png",
+  ];
 
-    IMAGES_IDLE = [
-        'img/2_character_pepe/1_idle/idle/I-1.png',
-        'img/2_character_pepe/1_idle/idle/I-2.png',
-        'img/2_character_pepe/1_idle/idle/I-3.png',
-        'img/2_character_pepe/1_idle/idle/I-4.png',
-        'img/2_character_pepe/1_idle/idle/I-5.png',
-        'img/2_character_pepe/1_idle/idle/I-6.png',
-        'img/2_character_pepe/1_idle/idle/I-7.png',
-        'img/2_character_pepe/1_idle/idle/I-8.png',
-        'img/2_character_pepe/1_idle/idle/I-9.png',
-        'img/2_character_pepe/1_idle/idle/I-10.png',
-    ];
-    
-    IMAGES_LONG_IDLE = [
-        'img/2_character_pepe/1_idle/long_idle/I-11.png',
-        'img/2_character_pepe/1_idle/long_idle/I-12.png',
-        'img/2_character_pepe/1_idle/long_idle/I-13.png',
-        'img/2_character_pepe/1_idle/long_idle/I-14.png',
-        'img/2_character_pepe/1_idle/long_idle/I-15.png',
-        'img/2_character_pepe/1_idle/long_idle/I-16.png',
-        'img/2_character_pepe/1_idle/long_idle/I-17.png',
-        'img/2_character_pepe/1_idle/long_idle/I-18.png',
-        'img/2_character_pepe/1_idle/long_idle/I-19.png',
-        'img/2_character_pepe/1_idle/long_idle/I-20.png',
-    ]
+  IMAGES_HURT = [
+    "img/2_character_pepe/4_hurt/H-41.png",
+    "img/2_character_pepe/4_hurt/H-42.png",
+    "img/2_character_pepe/4_hurt/H-43.png",
+  ];
 
-    world;
+  IMAGES_IDLE = [
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png",
+  ];
 
-    
-    constructor(){
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.audioManager = new AudioManager();
-        this.loadAllImages();
-        this.applyGravity();
-        this.animate();
+  IMAGES_LONG_IDLE = [
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png",
+  ];
+
+  world;
+
+  constructor() {
+    super().loadImage(this.IMAGES_WALKING[0]);
+    this.audioManager = new AudioManager();
+    this.loadAllImages();
+    this.applyGravity();
+    this.animate();
+  }
+
+  loadAllImages() {
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_JUMPING);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_LONG_IDLE);
+  }
+
+  animate() {
+    setInterval(() => {
+      this.characterMove();
+    }, 1000 / 60);
+
+    setInterval(() => {
+      this.chracterHurtDeadJumpAnimation();
+    }, 100);
+  }
+
+  chracterHurtDeadJumpAnimation() {
+    if (this.isDead()) {
+      this.playAnimation(this.IMAGES_DEAD);
+      openLoseScreen();
+    } else if (this.isHurt()) {
+      this.playAnimation(this.IMAGES_HURT);
+      this.audioManager.playHurtSound();
+    } else if (this.isAboveGround()) {
+      // jump animation
+      this.playAnimation(this.IMAGES_JUMPING);
+    } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+      // walk animation
+      this.playAnimation(this.IMAGES_WALKING);
+    } else {
+      this.characterIdle();
     }
+  }
 
-    loadAllImages() {
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
-        this.loadImages(this.IMAGES_JUMPING);
-        this.loadImages(this.IMAGES_IDLE);
-        this.loadImages(this.IMAGES_LONG_IDLE);
+  characterMove() {
+    this.audioManager.stopWalkingSound();
+    if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+      this.moveRight();
+      this.otherDirection = false;
+      this.audioManager.playWalkingSound();
+      this.lastMove = Date.now();
     }
-
-    animate() {
-        setInterval(() => {
-            this.characterMove();
-        }, 1000 / 60);
-
-        setInterval(() => {
-            this.chracterHurtDeadJumpAnimation();
-        }, 100);
+    if (this.world.keyboard.LEFT && this.x > 0) {
+      this.moveLeft();
+      this.otherDirection = true;
+      this.audioManager.playWalkingSound();
+      this.lastMove = Date.now();
     }
-
-    chracterHurtDeadJumpAnimation() {
-        if (this.isDead()) {
-            this.playAnimation(this.IMAGES_DEAD);
-            // openLoseScreen();
-        } else if (this.isHurt()) {
-            this.playAnimation(this.IMAGES_HURT);
-            this.audioManager.playHurtSound();
-        } else if (this.isAboveGround()) {
-            // jump animation
-            this.playAnimation(this.IMAGES_JUMPING);
-        } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // walk animation
-                this.playAnimation(this.IMAGES_WALKING);
-        } else {
-            this.characterIdle();
-        }
+    if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+      this.jump();
+      this.audioManager.playJumpSound();
+      this.lastMove = Date.now();
     }
+    this.world.camera_x = -this.x + 100;
+  }
 
-    characterMove() {
-        this.audioManager.stopWalkingSound();
-        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-            this.moveRight();
-            this.otherDirection = false;
-            this.audioManager.playWalkingSound();
-            this.lastMove = Date.now();
-        }
-        if (this.world.keyboard.LEFT && this.x > 0) {
-            this.moveLeft();
-            this.otherDirection = true;
-            this.audioManager.playWalkingSound();
-            this.lastMove = Date.now();
-        }
-        if (this.world.keyboard.SPACE && !this.isAboveGround()) {
-            this.jump();
-            this.audioManager.playJumpSound();
-            this.lastMove = Date.now();
-        }
-        this.world.camera_x = -this.x + 100;
+  characterIdle() {
+    if (Date.now() - this.lastMove >= 10000) {
+      this.playAnimation(this.IMAGES_LONG_IDLE);
+    } else {
+      this.playAnimation(this.IMAGES_IDLE);
     }
+  }
 
-    characterIdle() {
-        if (Date.now() - this.lastMove >= 10000) {
-            this.playAnimation(this.IMAGES_LONG_IDLE);
-        } else {
-            this.playAnimation(this.IMAGES_IDLE);
-        }
-    }
-
-    jump(){
-        this.speedY = 20;
-        this.lastMove = Date.now();
-    }
+  jump() {
+    this.speedY = 20;
+    this.lastMove = Date.now();
+  }
 }
--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -1,121 +1,120 @@
 class Endboss extends MovableObject {
-    height = 400;
-    width = 300;
-    y = 50;
+  height = 400;
+  width = 300;
+  y = 50;
 
-    IMAGES_WALKING = [
-        'img/4_enemie_boss_chicken/2_alert/G5.png',
-        'img/4_enemie_boss_chicken/2_alert/G6.png',
-        'img/4_enemie_boss_chicken/2_alert/G7.png',
-        'img/4_enemie_boss_chicken/2_alert/G8.png',
-        'img/4_enemie_boss_chicken/2_alert/G9.png',
-        'img/4_enemie_boss_chicken/2_alert/G10.png',
-        'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png',
-    ];
+  IMAGES_WALKING = [
+    "img/4_enemie_boss_chicken/2_alert/G5.png",
+    "img/4_enemie_boss_chicken/2_alert/G6.png",
+    "img/4_enemie_boss_chicken/2_alert/G7.png",
+    "img/4_enemie_boss_chicken/2_alert/G8.png",
+    "img/4_enemie_boss_chicken/2_alert/G9.png",
+    "img/4_enemie_boss_chicken/2_alert/G10.png",
+    "img/4_enemie_boss_chicken/2_alert/G11.png",
+    "img/4_enemie_boss_chicken/2_alert/G12.png",
+  ];
 
-    IMAGES_ALERT = [
-        'img/4_enemie_boss_chicken/2_alert/G5.png',
-        'img/4_enemie_boss_chicken/2_alert/G6.png',
-        'img/4_enemie_boss_chicken/2_alert/G7.png',
-        'img/4_enemie_boss_chicken/2_alert/G8.png',
-        'img/4_enemie_boss_chicken/2_alert/G9.png',
-        'img/4_enemie_boss_chicken/2_alert/G10.png',
-        'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png',
-    ];
+  IMAGES_ALERT = [
+    "img/4_enemie_boss_chicken/2_alert/G5.png",
+    "img/4_enemie_boss_chicken/2_alert/G6.png",
+    "img/4_enemie_boss_chicken/2_alert/G7.png",
+    "img/4_enemie_boss_chicken/2_alert/G8.png",
+    "img/4_enemie_boss_chicken/2_alert/G9.png",
+    "img/4_enemie_boss_chicken/2_alert/G10.png",
+    "img/4_enemie_boss_chicken/2_alert/G11.png",
+    "img/4_enemie_boss_chicken/2_alert/G12.png",
+  ];
 
-    IMAGES_ATTACK = [
-        'img/4_enemie_boss_chicken/3_attack/G13.png',
-        'img/4_enemie_boss_chicken/3_attack/G14.png',
-        'img/4_enemie_boss_chicken/3_attack/G15.png',
-        'img/4_enemie_boss_chicken/3_attack/G16.png',
-        'img/4_enemie_boss_chicken/3_attack/G17.png',
-        'img/4_enemie_boss_chicken/3_attack/G18.png',
-        'img/4_enemie_boss_chicken/3_attack/G19.png',
-        'img/4_enemie_boss_chicken/3_attack/G20.png',
-    ];
+  IMAGES_ATTACK = [
+    "img/4_enemie_boss_chicken/3_attack/G13.png",
+    "img/4_enemie_boss_chicken/3_attack/G14.png",
+    "img/4_enemie_boss_chicken/3_attack/G15.png",
+    "img/4_enemie_boss_chicken/3_attack/G16.png",
+    "img/4_enemie_boss_chicken/3_attack/G17.png",
+    "img/4_enemie_boss_chicken/3_attack/G18.png",
+    "img/4_enemie_boss_chicken/3_attack/G19.png",
+    "img/4_enemie_boss_chicken/3_attack/G20.png",
+  ];
 
-    IMAGES_HURT = [
-        'img/4_enemie_boss_chicken/4_hurt/G21.png',
-        'img/4_enemie_boss_chicken/4_hurt/G22.png',
-        'img/4_enemie_boss_chicken/4_hurt/G23.png',
-    ];
+  IMAGES_HURT = [
+    "img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
 
-    IMAGES_DEAD = [
-        'img/4_enemie_boss_chicken/5_dead/G24.png',
-        'img/4_enemie_boss_chicken/5_dead/G25.png',
-        'img/4_enemie_boss_chicken/5_dead/G26.png',
-    ];
+  IMAGES_DEAD = [
+    "img/4_enemie_boss_chicken/5_dead/G24.png",
+    "img/4_enemie_boss_chicken/5_dead/G25.png",
+    "img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
 
+  constructor() {
+    super().loadImage(this.IMAGES_WALKING[0]);
+    this.audioManager = new AudioManager();
+    this.loadAllImages();
+    this.x = 2500;
+    this.speed = 0.5;
+    this.animate();
+  }
 
-    constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.audioManager = new AudioManager();
-        this.loadAllImages();
-        this.x = 2500;
-        this.speed = 0.5;
-        this.animate();
+  loadAllImages() {
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_ATTACK);
+    this.loadImages(this.IMAGES_ALERT);
+  }
+
+  animate() {
+    let intervalEndboss = setInterval(() => {
+      this.endbossMove();
+      this.endbossAnimationDeadWalkHurtAttack();
+    }, 100);
+  }
+
+  endbossAnimationDeadWalkHurtAttack() {
+    this.endbossIsAlert();
+    if (this.isDead()) {
+      this.endbossDead();
+      openWinScreen();
+    } else if (this.isHurt() && this.energy <= 80) {
+      this.endbossIsHurt();
+    } else if (this.energy <= 80 && !this.isDead()) {
+      this.endbossAttack();
     }
+  }
 
-    loadAllImages() {
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
-        this.loadImages(this.IMAGES_ATTACK);
-        this.loadImages(this.IMAGES_ALERT);
-    }
+  endbossMove() {
+    this.audioManager.stopEndbossSound();
+    this.moveLeft();
+    this.playAnimation(this.IMAGES_WALKING);
+  }
 
-    animate(){
-        let intervalEndboss = setInterval(() => {
-            this.endbossMove();
-            this.endbossAnimationDeadWalkHurtAttack();
-        }, 100);
-    }
+  endbossIsHurt() {
+    this.audioManager.playEndbossSound();
+    this.speed = 8;
+    this.moveLeft();
+    this.playAnimation(this.IMAGES_HURT);
+  }
 
-    endbossAnimationDeadWalkHurtAttack() {
-        this.endbossIsAlert();
-        if (this.isDead()) {
-            this.endbossDead();
-            // openWinScreen()
-        } else if (this.isHurt() && this.energy <= 80) {
-            this.endbossIsHurt();
-        } else if (this.energy <= 80 && !this.isDead()) {
-            this.endbossAttack();
-        }
+  endbossIsAlert() {
+    if (this.world && this.world.endbossAlert === true) {
+      this.audioManager.playEndbossSound();
+      this.speed = 0;
+      this.playAnimation(this.IMAGES_ALERT);
     }
+  }
 
-    endbossMove() {
-        this.audioManager.stopEndbossSound();
-        this.moveLeft();
-        this.playAnimation(this.IMAGES_WALKING);
-    }
+  endbossAttack() {
+    this.audioManager.stopEndbossSound();
+    this.speed = 12;
+    this.moveLeft;
+    this.playAnimation(this.IMAGES_ATTACK);
+  }
 
-    endbossIsHurt() {
-        this.audioManager.playEndbossSound();
-        this.speed = 8;
-        this.moveLeft();
-        this.playAnimation(this.IMAGES_HURT);
-    }
-
-    endbossIsAlert() {
-        if (this.world && this.world.endbossAlert === true) {
-            this.audioManager.playEndbossSound();
-            this.speed = 0;
-            this.playAnimation(this.IMAGES_ALERT);
-        }
-    }
-
-    endbossAttack() {
-        this.audioManager.stopEndbossSound();
-        this.speed = 12;
-        this.moveLeft;
-        this.playAnimation(this.IMAGES_ATTACK);
-    }
-
-    endbossDead() {
-        this.audioManager.playChickenSound();
-        this.speed = 0;
-        this.playAnimation(this.IMAGES_DEAD);
-    }
+  endbossDead() {
+    this.audioManager.playChickenSound();
+    this.speed = 0;
+    this.playAnimation(this.IMAGES_DEAD);
+  }
 }
