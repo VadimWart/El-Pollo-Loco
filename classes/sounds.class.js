7acecc95a3c5cd4b6a5ce07c041be2d82c708df8
VadimWart <wartvadim@gmail.com>
2024-11-06 16:41:40 +0100

    game_level_update

--- classes/sounds.class.js
+++ classes/sounds.class.js
@@ -1,112 +1,112 @@
 class AudioManager {
-    constructor() {
-        this.walking_sound = new Audio('audio/running.mp3');
-        this.background_sound = new Audio('audio/background_sound.mp3');
-        this.chicken_sound = new Audio('audio/chicken_dead.mp3');
-        this.endboss_sound = new Audio('audio/endboss_dead.mp3');
-        this.game_over_sound = new Audio('audio/game_over.mp3');
-        this.hurt_sound = new Audio('audio/hurt_character.mp3');
-        this.jump_sound = new Audio('audio/jump.mp3');
-        this.win_sound = new Audio('audio/win.mp3');
-        this.coin_sound = new Audio('audio/coin.mp3');
-        this.pop_sound = new Audio('audio/pop_bottle.mp3');
-        this.splash_sound = new Audio('audio/bottle_splash.mp3');
-        this.idle_sound = new Audio('audio/idle.mp3');
-    }
+  constructor() {
+    this.walking_sound = new Audio("audio/running.mp3");
+    // this.background_sound = new Audio('audio/background_sound.mp3');
+    this.chicken_sound = new Audio("audio/chicken_dead.mp3");
+    this.endboss_sound = new Audio("audio/endboss_dead.mp3");
+    // this.game_over_sound = new Audio('audio/game_over.mp3');
+    this.hurt_sound = new Audio("audio/hurt_character.mp3");
+    this.jump_sound = new Audio("audio/jump.mp3");
+    // this.win_sound = new Audio('audio/win.mp3');
+    this.coin_sound = new Audio("audio/coin.mp3");
+    this.pop_sound = new Audio("audio/pop_bottle.mp3");
+    this.splash_sound = new Audio("audio/bottle_splash.mp3");
+    this.idle_sound = new Audio("audio/idle.mp3");
+  }
 
-    playSplashSound() {
-        this.splash_sound.play();
-    }
+  playSplashSound() {
+    this.splash_sound.play();
+  }
 
-    stopSplashSound() {
-        this.splash_sound.pause();
-    }
+  stopSplashSound() {
+    this.splash_sound.pause();
+  }
 
-    playIdleSound() {
-        this.idle_sound.play();
-    }
+  playIdleSound() {
+    this.idle_sound.play();
+  }
 
-    stopIdleSound() {
-        this.idle_sound.pause();
-    }
+  stopIdleSound() {
+    this.idle_sound.pause();
+  }
 
-    playPopSound() {
-        this.pop_sound.play();
-    }
+  playPopSound() {
+    this.pop_sound.play();
+  }
 
-    stopPopSound() {
-        this.pop_sound.pause();
-    }
+  stopPopSound() {
+    this.pop_sound.pause();
+  }
 
-    stopCoinSound() {
-        this.coin_sound.pause();
-    }
+  stopCoinSound() {
+    this.coin_sound.pause();
+  }
 
-    playCoinSound() {
-        this.coin_sound.play();
-    }
+  playCoinSound() {
+    this.coin_sound.play();
+  }
 
-    playWalkingSound() {
-        this.walking_sound.play();
-    }
+  playWalkingSound() {
+    this.walking_sound.play();
+  }
 
-    stopWalkingSound() {
-        this.walking_sound.pause();
-    }
+  stopWalkingSound() {
+    this.walking_sound.pause();
+  }
 
-    playBackgroundSound() {
-        this.background_sound.play();
-    }
+  playBackgroundSound() {
+    this.background_sound.play();
+  }
 
-    stopBackgroundSound() {
-        this.background_sound.pause();
-    }
+  stopBackgroundSound() {
+    this.background_sound.pause();
+  }
 
-    playChickenSound() {
-        this.chicken_sound.play();
-    }
+  playChickenSound() {
+    this.chicken_sound.play();
+  }
 
-    stopChickenSound() {
-        this.chicken_sound.pause();
-    }
+  stopChickenSound() {
+    this.chicken_sound.pause();
+  }
 
-    playEndbossSound() {
-        this.endboss_sound.play();
-    }
+  playEndbossSound() {
+    this.endboss_sound.play();
+  }
 
-    stopEndbossSound() {
-        this.endboss_sound.pause();
-    }
+  stopEndbossSound() {
+    this.endboss_sound.pause();
+  }
 
-    playGameOverSound() {
-        this.game_over_sound.play();
-    }
+  playGameOverSound() {
+    this.game_over_sound.play();
+  }
 
-    stopGameOverSound() {
-        this.game_over_sound.pause();
-    }
+  stopGameOverSound() {
+    this.game_over_sound.pause();
+  }
 
-    playHurtSound() {
-        this.hurt_sound.play();
-    }
+  playHurtSound() {
+    this.hurt_sound.play();
+  }
 
-    stopHurtSound() {
-        this.hurt_sound.pause();
-    }
+  stopHurtSound() {
+    this.hurt_sound.pause();
+  }
 
-    playJumpSound() {
-        this.jump_sound.play();
-    }
+  playJumpSound() {
+    this.jump_sound.play();
+  }
 
-    stopJumpSound() {
-        this.jump_sound.pause();
-    }
+  stopJumpSound() {
+    this.jump_sound.pause();
+  }
 
-    playWinSound() {
-        this.win_sound.play();
-    }
+  playWinSound() {
+    this.win_sound.play();
+  }
 
-    stopWinSound() {
-        this.win_sound.pause();
-    }
+  stopWinSound() {
+    this.win_sound.pause();
+  }
 }
--- js/game.js
+++ js/game.js
@@ -1,66 +1,242 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let isMuted = false;
 
+let background_sound = new Audio("audio/background_sound.mp3");
+let win_sound = new Audio("audio/win.mp3");
+let game_over_sound = new Audio("audio/game_over.mp3");
 
-
-function init(){
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-
-
-    console.log('My Character is', world.character);
-
+function init() {
+  startLevel();
+  canvas = document.getElementById("canvas");
+  world = new World(canvas, keyboard);
+  mobileButtons();
 }
 
 window.addEventListener("keydown", (e) => {
-    if(e.keyCode == 39) {
-        keyboard.RIGHT = true;
-    }
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
 
-    if(e.keyCode == 37) {
-        keyboard.LEFT = true;
-    }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
 
-    if(e.keyCode == 38) {
-        keyboard.UP = true;
-    }
+  if (e.keyCode == 38) {
+    keyboard.UP = true;
+  }
 
-    if(e.keyCode == 40) {
-        keyboard.DOWN = true;
-    }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
 
-    if(e.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
 
-    if(e.keyCode == 68) {
-        keyboard.D = true;
-    }
+  if (e.keyCode == 68) {
+    keyboard.D = true;
+  }
 });
 
 window.addEventListener("keyup", (e) => {
-    if(e.keyCode == 39) {
-        keyboard.RIGHT = false;
-    }
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
 
-    if(e.keyCode == 37) {
-        keyboard.LEFT = false;
-    }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
 
-    if(e.keyCode == 38) {
-        keyboard.UP = false;
-    }
+  if (e.keyCode == 38) {
+    keyboard.UP = false;
+  }
 
-    if(e.keyCode == 40) {
-        keyboard.DOWN = false;
-    }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
 
-    if(e.keyCode == 32) {
-        keyboard.SPACE = false;
-    }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
 
-    if(e.keyCode == 68) {
-        keyboard.D = false;
-    }
+  if (e.keyCode == 68) {
+    keyboard.D = false;
+  }
 });
+
+function mobileButtons() {
+  document.getElementById("btnLeft").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.LEFT = true;
+  });
+  document.getElementById("btnLeft").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.LEFT = false;
+  });
+  document.getElementById("btnRight").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.RIGHT = true;
+  });
+  document.getElementById("btnRight").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.RIGHT = false;
+  });
+  document.getElementById("btnJump").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.SPACE = true;
+  });
+  document.getElementById("btnJump").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.SPACE = false;
+  });
+  document.getElementById("btnThrow").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.THROW = true;
+  });
+  document.getElementById("btnThrow").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.THROW = false;
+  });
+}
+
+function pauseSounds() {
+  background_sound.pause();
+  background_sound.currentTime = 0;
+  win_sound.pause();
+  win_sound.currentTime = 0;
+  game_over_sound.pause();
+  game_over_sound.currentTime = 0;
+}
+
+function startLevel() {
+  initLevel();
+}
+
+function startGame() {
+  let startScreen = document.getElementById("startScreen");
+  let endScreenWin = document.getElementById("endScreenWin");
+  let endScreenLose = document.getElementById("endScreenLose");
+  init();
+  background_sound.play();
+  startScreen.style.display = "none";
+  endScreenWin.style.display = "none";
+  endScreenLose.style.display = "none";
+}
+
+function restartGame() {
+  clearAllIntervals();
+  pauseSounds();
+
+  let endScreenWin = document.getElementById("endScreenWin");
+  let endScreenLose = document.getElementById("endScreenLose");
+  let startScreen = document.getElementById("startScreen");
+  //   let instructions = document.getElementById("instructions");
+
+  endScreenWin.style.display = "none";
+  endScreenLose.style.display = "none";
+  // instructions.style.display = "none";
+  startScreen.style.display = "none";
+
+  init();
+  background_sound.play();
+}
+
+function goToMenu() {
+  clearAllIntervals();
+  pauseSounds();
+
+  let startScreen = document.getElementById("startScreen");
+  let endScreenWin = document.getElementById("endScreenWin");
+  let endScreenLose = document.getElementById("endScreenLose");
+
+  endScreenWin.style.display = "none";
+  endScreenLose.style.display = "none";
+  startScreen.style.display = "block";
+}
+
+function openWinScreen() {
+  clearAllIntervals();
+  background_sound.pause();
+  win_sound.play();
+  let endScreenWin = document.getElementById("endScreenWin");
+  endScreenWin.style.display = "flex";
+}
+
+function openLoseScreen() {
+  clearAllIntervals();
+  background_sound.pause();
+  game_over_sound.play();
+  let endScreenLose = document.getElementById("endScreenLose");
+  endScreenLose.style.display = "flex";
+}
+
+function clearAllIntervals() {
+  for (let i = 1; i < 9999; i++) {
+    window.clearInterval(i);
+  }
+}
+
+function toggleMute() {
+  const muteButton = document.querySelector(".button-mute");
+  const img = muteButton.querySelector("img");
+  const mobileMuteBtn = document.getElementById("muteBtn");
+
+  if (!isMuted) {
+    img.src = "./img_pollo_locco/img/lautsprecher.png";
+    mobileMuteBtn.innerHTML = "UNMUTE";
+    background_sound.muted = true;
+    win_sound.muted = true;
+    game_over_sound.muted = true;
+    world.audioManager.muteGameSounds();
+    world.character.audioManager.muteGameSounds();
+    world.level.endboss.forEach((endboss) => {
+      endboss.audioManager.muteGameSounds();
+    });
+    isMuted = true;
+  } else {
+    img.src = "./img_pollo_locco/img/lautsprecher-aus.png";
+    mobileMuteBtn.innerHTML = "MUTE";
+    background_sound.muted = false;
+    win_sound.muted = false;
+    game_over_sound.muted = false;
+    world.audioManager.unmuteGameSounds();
+    world.character.audioManager.unmuteGameSounds();
+    world.level.endboss.forEach((endboss) => {
+      endboss.audioManager.unmuteGameSounds();
+    });
+    isMuted = false;
+  }
+}
+
+function checkOrientation() {
+  const landscapeScreen = document.getElementById("landscapeScreen");
+  const gameContainer = document.querySelector(".game-container");
+  const overlay = document.querySelector(".overlay");
+  const muteBtn = document.querySelector(".button-mute");
+
+  if (window.innerWidth < 1200 && window.innerHeight < window.innerWidth) {
+    landscapeScreen.style.display = "none";
+    muteBtn.style.display = "none";
+    gameContainer.style.display = "block";
+    overlay.style.display = "flex";
+  } else if (
+    window.innerWidth < 1200 &&
+    window.innerHeight > window.innerWidth
+  ) {
+    landscapeScreen.style.display = "flex";
+    muteBtn.style.display = "none";
+    gameContainer.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    landscapeScreen.style.display = "none";
+    muteBtn.style.display = "block";
+    gameContainer.style.display = "block";
+    overlay.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", checkOrientation);
+window.addEventListener("orientationchange", checkOrientation);
+window.addEventListener("load", checkOrientation);
