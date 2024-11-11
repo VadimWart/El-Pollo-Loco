4c68e19315361fe227d058d02ed8db76e48717b0
VadimWart <wartvadim@gmail.com>
2024-11-11 21:56:00 +0100

    volume_add

--- classes/sounds.class.js
+++ classes/sounds.class.js
@@ -1,14 +1,31 @@
 class AudioManager {
   constructor() {
     this.walking_sound = new Audio("audio/running.mp3");
+    this.walking_sound.volume = 0.1;
+
     this.chicken_sound = new Audio("audio/chicken_dead.mp3");
+    this.chicken_sound.volume = 0.1;
+
     this.endboss_sound = new Audio("audio/endboss_dead.mp3");
+    this.endboss_sound.volume = 0.1;
+
     this.hurt_sound = new Audio("audio/hurt_character.mp3");
+    this.hurt_sound.volume = 0.1;
+
     this.jump_sound = new Audio("audio/jump.mp3");
+    this.jump_sound.volume = 0.1;
+
     this.coin_sound = new Audio("audio/coin.mp3");
+    this.coin_sound.volume = 0.1;
+
     this.pop_sound = new Audio("audio/pop_bottle.mp3");
+    this.pop_sound.volume = 0.1;
+
     this.splash_sound = new Audio("audio/bottle_splash.mp3");
+    this.splash_sound.volume = 0.1;
+
     this.idle_sound = new Audio("audio/idle.mp3");
+    this.idle_sound.volume = 0.1;
   }
 
   playSplashSound() {
