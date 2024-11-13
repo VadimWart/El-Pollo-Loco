62bd7182292d035274d3387653212772e78dfb6c
VadimWart <wartvadim@gmail.com>
2024-11-13 23:58:26 +0100

    add_clouds_class_JSDocs

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,19 +1,24 @@
 class Cloud extends MovableObject {
-    width = 500;
-    height = 250;
+  width = 500;
+  height = 250;
 
-    constructor(){
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 2400; // cloud range
-        this.y = 10 + Math.random() * 30;
-        this.animate();
-        
-    }
+  /**
+   * Initializes a new Cloud instance with a random position and starts the animation.
+   */
+  constructor() {
+    super().loadImage("img/5_background/layers/4_clouds/1.png");
+    this.x = Math.random() * 2400; // cloud range
+    this.y = 10 + Math.random() * 30;
+    this.animate();
+  }
 
-    animate() { // wolken animation
-        setInterval(() => {
-            this.moveLeft(); // CLoud Direction
-        }, 1000 / 60); // speed
-    }
-
+  /**
+   * Starts a continuous animation moving the cloud to the left.
+   * The animation updates 60 times per second to create smooth movement.
+   */
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+  }
 }
