ff682541d50c4d5aeb6d53c323c26a56fda16a13
VadimWart <wartvadim@gmail.com>
2024-11-14 00:02:22 +0100

    add_drawable_object_class_JSDocs

--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -1,31 +1,40 @@
 class DrawableObject {
-    img;
-    imageCache = {}; // JSON/Objekt um bilder abzuspeichern, charachter und chicken animieren
-    currentImage = 0;
-    x = 120;
-    y = 280;
-    height = 150;
-    width = 100;
+  img;
+  imageCache = {}; // JSON/Objekt um bilder abzuspeichern, charachter und chicken animieren
+  currentImage = 0;
+  x = 120;
+  y = 280;
+  height = 150;
+  width = 100;
 
+  /**
+   * Loads an image from the provided path and stores it in the `img` property.
+   * @param {string} path - The file path to the image to be loaded (e.g., 'img/image.png').
+   */
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-    
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+  /**
+   * Draws the loaded image onto the canvas context at the object's position and size.
+   * @param {CanvasRenderingContext2D} ctx - The 2D rendering context of the canvas where the image will be drawn.
+   */
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
 
-    /**
-    * 
-    * @param {Array} arr - ['img/image1.png', 'img/image2.png', ... ] 
-    */
-    loadImages(arr){
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  /**
+   * Loads multiple images from an array of paths and stores them in the `imageCache` for later use.
+   * @param {Array<string>} arr - An array of image file paths (e.g., ['img/image1.png', 'img/image2.png', ...]).
+   * @example
+   * loadImages(['img/image1.png', 'img/image2.png']);
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 }
