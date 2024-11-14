49eefd1cc8c524e1dd5f97d6553e07a2767dc863
VadimWart <wartvadim@gmail.com>
2024-11-15 00:37:00 +0100

    add_status_bar_class_JSDocs

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,49 +1,57 @@
 class StatusBar extends DrawableObject {
-    x = 50;
-    y = 0;
-    width = 200;
-    height = 60;
-    percentage = 100;
+  x = 50;
+  y = 0;
+  width = 200;
+  height = 60;
+  percentage = 100;
 
-    IMAGES = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
+  IMAGES = [
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png",
+  ];
 
-    ];
+  /**
+   * Initializes the ProgressImage instance.
+   * Loads all images into the cache and sets the initial display to 100%.
+   */
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES);
+    this.setPercentage(100);
+  }
 
+  /**
+   * Sets the current percentage and updates the displayed image based on the percentage.
+   * @param {number} percentage - The progress or health level, ranging from 0 to 100.
+   */
+  setPercentage(percentage) {
+    this.percentage = percentage;
+    let path = this.IMAGES[this.resolveImageIndex()];
+    this.img = this.imageCache[path];
+  }
 
-    constructor() {
-        super();
-        // Lade alle Bilder ins imageCache
-        this.loadImages(this.IMAGES);
-        this.setPercentage(100); // Setze das anfängliche Bild basierend auf dem Prozentsatz
+  /**
+   * Determines the appropriate image index based on the percentage.
+   * Returns an index to select the right image from the `IMAGES` array.
+   * @returns {number} - The index of the appropriate image for the current percentage.
+   */
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
     }
-
-    // Setzt den Prozentsatz und wählt das entsprechende Bild aus dem Cache
-    setPercentage(percentage) {
-        this.percentage = percentage; // => 0 ... 5
-        let path = this.IMAGES[this.resolveImageIndex()];
-        this.img = this.imageCache[path]; // Hole das Bild aus dem Cache
-    }
-
-    // Bestimmt das entsprechende Bild basierend auf dem Prozentsatz
-    resolveImageIndex() {
-        if(this.percentage == 100) {
-            return 5;
-        } else if (this.percentage > 80) {
-            return 4;
-        } else if (this.percentage > 60) {
-            return 3;
-        } else if (this.percentage > 40) {
-            return 2;
-        } else if (this.percentage > 20) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+  }
 }
